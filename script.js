475859c917314ed45b94d6662c85769a0fd34c34
amanmaharjan11 <nepes17190@lurenwu.com>
2022-09-07 21:03:50 +0545

    my third commit

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 // collecting form data
+function getInfo(){
 
-let contact = document.getElementById('contact-name').value;
-console.log(contact);
-let email = document.getElementById('contact-email').value;
-console.log(email);
-let message = document.getElementById('contact-message').value;
-console.log(message);
+    let contact = document.getElementById('contact-name').value;
+    console.log(contact);
+    let email = document.getElementById('contact-email').value;
+    console.log(email);
+    let message = document.getElementById('send-message').value;
+    console.log(message);
+    let btn = document.getElementById("btn").value;
+    console.log(btn);
+
+}
+
+document.getElementById("btn").addEventListener("click", getInfo);
+
+
+
+
+// (()=> {})
