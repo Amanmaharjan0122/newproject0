ab00dac3f27e1f41a4419d609b480b08817be18d
amanmaharjan11 <nepes17190@lurenwu.com>
2022-09-07 19:47:38 +0545

    my first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// collecting form data
+
+let contact = document.getElementById('contact-name').value;
+console.log(contact);
+let email = document.getElementById('contact-email').value;
+console.log(email);
+let message = document.getElementById('contact-message').value;
+console.log(message);
